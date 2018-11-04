6d464703618635073d50ebbfc44bcd9628c210bc
aleksei.dudko@gmail.com <aleksei.dudko@gmail.com>
2018-11-04 18:26:49 +0300

    Implement sending and receiving simple messages

--- RabbitMqClient/Program.cs
+++ RabbitMqClient/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using RabbitMQ.Client;
 
 namespace RabbitMqClient
 {
@@ -10,6 +8,33 @@ namespace RabbitMqClient
     {
         static void Main(string[] args)
         {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "DocQueue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "DocQueue",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
